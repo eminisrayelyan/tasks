8a38e05d66dd283f3be78bcfe15d31cbbfd82256
eminisrayelyan <israyelyan.emin@gmail.com>
2026-09-10 10:42:22 +0400

    perf: make some performance optimizations

--- week2-react/src/App.jsx
+++ week2-react/src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import { lazy } from 'react'
 import ProductCard from './components/ProductCard.jsx'
-import ProductPage from './components/ProductPage.jsx'
+const ProductPage = lazy(() => import('./components/ProductPage.jsx'))
 import Header from './components/Header.jsx'
 
 function App() {
   return (
     <div className='bg-[#F7FAFE]'>
-       <Header></Header>
-       <Routes>
-          <Route path='/' element={<ProductCard/>}/>
-          <Route path='/product/:id'element={<ProductPage/>}/>
-       </Routes>  
-    </div> 
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<ProductCard />} />
+        <Route path='/product/:id' element={<ProductPage />} />
+      </Routes>
+    </div>
   );
 }
 
