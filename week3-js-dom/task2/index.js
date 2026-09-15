ff0860b0c6292bee3906d3c29d5a7ebe18e2f76b
eminisrayelyan <israyelyan.emin@gmail.com>
2026-09-15 17:00:38 +0400

    refactor: simplify nested if statements

--- week3-js-dom/task2/index.js
+++ week3-js-dom/task2/index.js
@@ -1,35 +1,34 @@
 const input = document.querySelector('.input');
+const button = document.querySelector('.button')
 
 function handleClcik(e) {
-    if (e.target.matches('.button')) {
-        if (input.textContent.length <= 10) {
-            if (e.target.matches('.plus, .minus, .mul, .division')) {
-                const last = input.textContent.at(-1);
-
-                if ('+-*/'.includes(last) || input.textContent === '') {
-                    return;
-                }
-            }
-
-            if (e.target.matches('.clear')) {
-                input.textContent = '';
-            }
-
-            else if (e.target.matches('.equal')) {
-                input.textContent = eval(input.textContent);
-            } else {
-                input.textContent += e.target.innerText
-            }
-        } else {
-            if (e.target.matches('.equal')) {
-                input.textContent = eval(input.textContent);
-            } else if (e.target.matches('.clear')) {
-                input.textContent = '';
-            }
-
-            return;
-        }
+    if (!e.target.matches('.button')) {
+        return;
     }
+
+    if (e.target.matches('.clear')) {
+        input.textContent = '';
+        return;
+    }
+
+    if (e.target.matches('.equal')) {
+        input.textContent = eval(input.textContent);
+        return;
+    }
+
+    const isOperator = e.target.matches('.plus, .minus, .mul, .division');
+    const last = input.textContent.at(-1);
+
+    if (input.textContent.length >= 10) {
+        return;
+    }
+            
+    if (isOperator && (input.textContent === '' || '+-*/'.includes(last))) {
+        return;
+    } 
+     
+    input.textContent += e.target.innerText;
+    
 }
 
 document.addEventListener('click', handleClcik);
